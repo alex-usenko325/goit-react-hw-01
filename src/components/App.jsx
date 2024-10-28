0a6adaa9200ba2c56da3be0cde8e3f6ca704d995
Zergus325 <alex.usenko.97@gmail.com>
2024-10-28 18:00:01 +0200

    add task 3

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
-import Profile from './Profile/Profile';
-import FriendList from './FriendList/FriendList';
-import userData from '../userData.json';
-import friends from '../friends.json';
+import Profile from "./Profile/Profile";
+import FriendList from "./FriendList/FriendList";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+
+import userData from "../userData.json";
+import friends from "../friends.json";
+import transactions from "../transactions.json";
 
 const App = () => {
   return (
@@ -14,6 +17,7 @@ const App = () => {
         stats={userData.stats}
       />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 };
